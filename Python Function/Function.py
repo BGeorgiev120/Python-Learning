1586e77bb147deeb08d541d96f0913a2162d3307
BGeorgiev120 <bojidarg2012g@gmail.com>
2025-04-30 16:26:51 +0300

    Python Functions & While

--- Python Function/Function.py
+++ Python Function/Function.py
@@ -15,4 +15,15 @@ while i < 5:
     example()
     i +=1
 # If you dont understand the While code make sure  to see Python While folder!
+
+print(" ") # This is just simple space
+
+# Now I will show you how you can use these () to add temporary variables
+def example_2(name, date):
+    print(f"Hey the name set in the argument is {name}")
+    print(f"The date today is {date}")
+
+example_2("John", "01.05.2025")
+
 # While can be used for so many things as good example for the functions.
+
