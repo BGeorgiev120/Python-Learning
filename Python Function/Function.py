aab00f9d1fe22580a5aef018f1895c32a46c9c51
BGeorgiev120 <bojidarg2012g@gmail.com>
2025-04-30 16:07:36 +0300

    Python Functions & While

---
+++ Python Function/Function.py
@@ -0,0 +1,24 @@
+# function = A block of code that can be runned whenever you want.
+# The functions are usefull for spending less time on the same code.
+# Here is how to call a function: function_name()
+
+# Now lest create simple function:
+def example():
+    if i == 1:
+
+        print(f"Hey that is Hey for first time!")
+    else:
+        print(f"Hey thay is Hey for {i} times")
+
+i = 1
+while i < 5:
+    example()
+    i +=1
+# If you dont understand the While code make sure  to see Python While folder!
+# While can be used for so many things as good example for the functions.
+i = 20
+if i == 10 or i==1:
+    print(f"The value of 'i' is {i}")
+
+else:
+    print(f"The veriable 'i' is not number 1 or number 10")
---
+++ Python While/while.py
@@ -0,0 +1,9 @@
+# In this cenario we use white to repeat some messages. This can be used for everything!
+# Make sure you have the correct sytax or the code will be broken.
+i = 10
+while i > 0:
+    print(f"The number {i} is bigger then 0")
+    i -= 1
+    
+# In this scenario we have While that makes whenever 'i' is bigger then 0 to make it smaller by one!
+# While can check,repeat and do more things. You just need to have imagination.
